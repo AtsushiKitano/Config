684e3d962f3f640e74ea109221c93f4a3956e4f0
AtsushiKitano <atsushi@aquamarine-cloud.net>
2022-05-01 16:31:47 +0900

    update

--- Emacs/configs/init/python.el
+++ Emacs/configs/init/python.el
@@ -1,4 +1,4 @@
-(add-hook 'python-mode-hook 'flycheck-mode)
+;; (add-hook 'python-mode-hook 'flycheck-mode)
 (add-hook 'python-mode-hook
           (lambda()
             (setq python-indent 2)
--- Emacs/configs/user-conf/go.el
+++ Emacs/configs/user-conf/go.el
@@ -1,3 +1,6 @@
+(setenv "GOROOT" "/Users/kitano.atsushi/.goenv/versions/1.17.7")
+(setenv "GOPATH" "/Users/kitano.atsushi/go/1.17.7")
+
 (use-package go-mode
   :mode "\\.go\\'"
   :interpreter "go"
@@ -8,6 +11,40 @@
                             (local-set-key (kbd "M-.") 'godef-jump)
                             (setq indent-tabs-mode nil)
                             (setq c-basic-offset 4)
-                            (setq tab-width 4)
-                            )
-  )
+                            (setq tab-width 4))))
+
+;;; Setup
+
+(require 'company)                                   ; load company mode
+(require 'company-go)                                ; load company mode go backend
+
+;;; Possible improvements
+
+(setq company-tooltip-limit 20)                      ; bigger popup window
+(setq company-idle-delay .3)                         ; decrease delay before autocompletion popup shows
+(setq company-echo-delay 0)                          ; remove annoying blinking
+(setq company-begin-commands '(self-insert-command)) ; start autocompletion only after typing
+
+;;; Only use company-mode with company-go in go-mode
+
+(add-hook 'go-mode-hook (lambda ()
+                          (set (make-local-variable 'company-backends) '(company-go))
+                          (company-mode)))
+
+;;; Color customization
+
+(custom-set-faces
+ '(company-preview
+   ((t (:foreground "darkgray" :underline t))))
+ '(company-preview-common
+   ((t (:inherit company-preview))))
+ '(company-tooltip
+   ((t (:background "lightgray" :foreground "black"))))
+ '(company-tooltip-selection
+   ((t (:background "steelblue" :foreground "white"))))
+ '(company-tooltip-common
+   ((((type x)) (:inherit company-tooltip :weight bold))
+    (t (:inherit company-tooltip))))
+ '(company-tooltip-common-selection
+   ((((type x)) (:inherit company-tooltip-selection :weight bold))
+    (t (:inherit company-tooltip-selection)))))
